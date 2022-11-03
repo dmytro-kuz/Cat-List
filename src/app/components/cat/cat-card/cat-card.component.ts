842275d78892c485e54949a84be0f4345e16a062
dmytro <kuzdsma1998@gmail.com>
2022-11-03 14:22:58 +0200

    create adaptive card

--- src/app/components/cat/cat-card/cat-card.component.ts
+++ src/app/components/cat/cat-card/cat-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cat } from 'src/app/shared/interfaces/Cat';
 
 @Component({
   selector: 'app-cat-card',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatCardComponent implements OnInit {
   constructor() {}
+
+  @Input() catCard?: Cat;
 
   ngOnInit() {}
 }
